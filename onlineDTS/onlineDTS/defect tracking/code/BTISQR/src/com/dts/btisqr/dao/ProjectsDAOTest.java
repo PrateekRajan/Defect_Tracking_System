1d2810903f66c2a7d6ec8bd7cda97897e1da938d
Prateek Rajan <prateek.rajan89@gmail.com>
2013-03-03 17:28:35 -0600

    Final Commit

---
+++ onlineDTS/onlineDTS/defect tracking/code/BTISQR/src/com/dts/btisqr/dao/ProjectsDAOTest.java
@@ -0,0 +1,79 @@
+package com.dts.btisqr.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProjectsDAOTest {
+
+	@Test
+	public void testAddProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetManagerProjects() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProjectDetails() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAssignProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProjectMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFreeMemebers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProperties() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProperties() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConnection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSequenceID() {
+		fail("Not yet implemented");
+	}
+
+}
